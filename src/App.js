9ece4823b2a1a26b29ee789916f168b7b3029ee2
Rostyk Podolets <ros.podolets@gmail.com>
2022-01-02 06:53:21 +0200

    tt subtask-1 checkpoint-1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,52 @@
-function App() {
-  return <div className="App">TEST</div>;
+import React, { Component } from 'react';
+import { nanoid } from 'nanoid';
+
+class App extends Component {
+  state = {
+    contacts: [],
+    name: '',
+  };
+
+  handleInput = event => {
+    // console.log(event.target.value);
+    // console.log(event.target.name);
+    this.setState({ name: event.target.value });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    // console.log(event.target);
+    const { name } = this.state;
+
+    const newContact = {
+      id: nanoid(),
+      name,
+    };
+
+    this.setState(prevState => ({
+      contacts: [newContact, ...prevState.contacts],
+    }));
+  };
+
+  render() {
+    return (
+      <>
+        <h1>Phonebook</h1>
+        <form onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleInput}
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+          />
+          <button type="submit">Add contact</button>
+        </form>
+        <h2>Contacts</h2>
+      </>
+    );
+  }
 }
 
 export default App;
