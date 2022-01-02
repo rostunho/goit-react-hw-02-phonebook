1defc920d65e5bc6e083dffa3f0ca8b6cfb1f7c0
Rostyk Podolets <ros.podolets@gmail.com>
2022-01-02 08:13:15 +0200

    tt subtask-2 completed

--- src/App.js
+++ src/App.js
@@ -5,34 +5,37 @@ class App extends Component {
   state = {
     contacts: [],
     name: '',
+    number: '',
   };
 
   handleInput = event => {
     // console.log(event.target.value);
     // console.log(event.target.name);
-    this.setState({ name: event.target.value });
+    const { name, value } = event.target;
+
+    this.setState({ [name]: value });
   };
 
   handleSubmit = event => {
     event.preventDefault();
     // console.log(event.target);
-    const { name } = this.state;
+    const { name, number } = this.state;
 
     const newContact = {
       id: nanoid(),
       name,
+      number,
     };
 
     this.setState(prevState => ({
       contacts: [newContact, ...prevState.contacts],
       name: '',
+      number: '',
     }));
-
-    // this.setState({ name: '' });
   };
 
   render() {
-    const { contacts, name } = this.state;
+    const { contacts, name, number } = this.state;
 
     return (
       <>
@@ -47,12 +50,24 @@ class App extends Component {
             title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
             required
           />
+          <input
+            type="tel"
+            name="number"
+            value={number}
+            onChange={this.handleInput}
+            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+            required
+          />
+
           <button type="submit">Add contact</button>
         </form>
         <h2>Contacts</h2>
         <ul>
-          {contacts.map(({ id, name }) => (
-            <li key={id}>{name}</li>
+          {contacts.map(({ id, name, number }) => (
+            <li key={id}>
+              {name}: {number}
+            </li>
           ))}
         </ul>
       </>
